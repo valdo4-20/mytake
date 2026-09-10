f913565ffbb9acbe968025720e6af201e60f17b5
Valdo <osvaldooliveira361@gmail.com>
2026-09-10 06:45:52 -0300

    Atualiza service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'mytake-v21';
+const CACHE = 'mytake-v22';
 const CORE = ['./', './index.html', './styles.css?v=3', './icon.svg', './manifest.webmanifest', './src/app.js?v=14', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
