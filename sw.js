b351384fb28c2259b84a3bac328557968c963d0a
Valdo <osvaldooliveira361@gmail.com>
2026-09-10 11:30:23 -0300

    Adiciona estante 3D na biblioteca

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mytake-v41';
-const CORE = ['./', './index.html', './styles.css?v=9', './icon.svg', './manifest.webmanifest', './src/app.js?v=32', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
+const CACHE = 'mytake-v42';
+const CORE = ['./', './index.html', './styles.css?v=9', './icon.svg', './manifest.webmanifest', './src/app.js?v=33', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
