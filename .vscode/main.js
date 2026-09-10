a8cc534eb7b6ce9c6e958d63ac35f0d2c89034b4
Valdo <osvaldooliveira361@gmail.com>
2026-09-10 11:13:15 -0300

    Corrige travamento ao personalizar perfil

---
+++ .vscode/main.js
@@ -0,0 +1,13 @@
+/// <reference path="c:/Users/osval/.vscode/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
+/// <reference path="c:/Users/osval/.vscode/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
+//  @ts-check
+//  API: https://code.visualstudio.com/api/references/vscode-api
+
+/** @param {object} _context */
+function activate(_context) {
+   window.showInformationMessage('Hello, World!');
+}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate }
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mytake-v37';
-const CORE = ['./', './index.html', './styles.css?v=9', './icon.svg', './manifest.webmanifest', './src/app.js?v=28', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
+const CACHE = 'mytake-v39';
+const CORE = ['./', './index.html', './styles.css?v=9', './icon.svg', './manifest.webmanifest', './src/app.js?v=30', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
