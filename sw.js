327c4a72d85de5ff870b82977eddbade7252ff87
Valdo <osvaldooliveira361@gmail.com>
2026-09-10 11:50:46 -0300

    Corrige foco e atualiza cache do formulario

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mytake-v44';
-const CORE = ['./', './index.html', './styles.css?v=11', './icon.svg', './manifest.webmanifest', './src/app.js?v=33', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
+const CACHE = 'mytake-v45';
+const CORE = ['./', './index.html', './styles.css?v=11', './icon.svg', './manifest.webmanifest', './src/app.js?v=34', './src/data.js', './src/storage.js', './src/diary.js', './src/tmdb.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
